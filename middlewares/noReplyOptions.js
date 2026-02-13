197a514181c7b75ab363ac4aea13446483634c49
Felixzandamela <81531283+Felixzandamela@users.noreply.github.com>
2026-02-13 13:54:50 +0200

    update middlewares/noReplyOptions.js

--- middlewares/noReplyOptions.js
+++ middlewares/noReplyOptions.js
@@ -3,16 +3,16 @@ const {getTime,formatDate} = require('./utils');
 const baseUrl = process.env.HOST;
 const protocol = process.env.PROTOCAL;
 // no-reply emails options
-module.exports.noReplyOptions = async function(user, type){
+module.exports.noReplyOptions = async function(item, type){
   const companyName = process.env.COMPANY;
   const datas = {
     verifyaccount:{
-      path: `${protocol}${baseUrl}/auth/account-verification?id=${user._id}`,
+      path: `${protocol}${baseUrl}/auth/account-verification?id=${item._id}`,
       subject: "Verifique sua conta "+companyName,
       template: 'emails/account-verification'
     },
     resetpassword:{
-      path: `${protocol}${baseUrl}/auth/reset-password?token=${user._id}`,
+      path: `${protocol}${baseUrl}/auth/reset-password?token=${item._id}`,
       subject: "Você solicitou alteração da senha",
       template: 'emails/reset-password'
     },  
@@ -30,25 +30,31 @@ module.exports.noReplyOptions = async function(user, type){
       path: `${protocol}${baseUrl}/admin/alerts`,
       subject: "Uma tentativa de navegação na area de administração!",
       template: 'emails/unauthorizedNavigator'
+    },
+    withdrawalsFunds:{
+      path: `${protocol}${baseUrl}/cabinet/transactions/deposits/view?_id=${item._id}`,
+      subject: "Saque concluido com sucesso!",
+      template: 'emails/withdrawalsFunds'
     }
   }
   return{
     from: ''+companyName+'"no-replay@avvimo.com"', // sender address
-    to: user.email,
+    to: item.email,
     subject: datas[type].subject,
     template: datas[type].template, 
     context:{
       logo: `${protocol}${baseUrl}/imgs/logo1.png`,
-      name: user.name,
-      email: user.email,
-      guestEmail: user.guest? user.guest.email : "",
+      name: item.name,
+      email: item.email,
+      guestEmail: item.guest? item.guest.email : "",
+      amount: item.amount? item.amount : "",
       time: formatDate().fullDate,
       companyName: companyName,
       location:"1120 Facim, Marracuene Maputo, Moçambique",
       link: datas[type].path,
       year: getTime().onlyYear,
       baseLink: `${protocol}${baseUrl}`,
-      supportUrl: `${protocol}${baseUrl}/contact-us?id=${user._id}`
+      supportUrl: `${protocol}${baseUrl}/contact-us?id=${item._id}`
     }
   }    
 }
